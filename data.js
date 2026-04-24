7eeceff61271724f1c75f5576d163321ce50ff22
jxcalvn <juancalviano1@gmail.com>
2026-04-24 09:22:32 +0700

    Upload GitHub pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "": {
+        judul: "Sistem POS untuk PT Jala ninajadulu",
+        keahlian: ["Rayap Besi"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzha-VE2Cg55oV2eENCI6hu-ITGPdE8XNV5Q&s",
+        latar: "",
+        tantangan: "",
+        pelajaran: "",
+        link: ""
+    },
+};
